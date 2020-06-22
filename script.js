c660ac457ad9ffe8317f847a2cbee6b258682663
Ebraheem Labib <lolhema1995@gmail.com>
2020-06-22 06:31:44 +0200

    getting items

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 
+document.querySelector('.control-buttons span').onclick = function() {
 
-var value = document.getElementById('val');
-var result= document.getElementById('result');
-var Calculate= document.getElementById('Calc');
+let yourname = prompt("What is Your Name");
+
+  if(yourname == null || yourname == "" ){
+
+      document.querySelector('.name span').innerHTML = 'Unkown';
+
+  }
+
+  else{
+
+    document.querySelector('.name span').innerHTML = yourname;
+
+  }
 
 
-
-value.onkeyup = function(){
-
-result.innerHTML="Calculating";
-
-};
-
-value.onkeydown = function(){
-
-result.innerHTML="  ";
-
-};
-
-Calculate.onclick = function() {
-
-  result.innerHTML= value.value *3 +" "+"in USD";
-
+  document.querySelector(".control-buttons").remove();
 
 };
+
+let duration =1000; 
+
+
+let blockscontainer =document.querySelector('.memory-game-blocks');
+
+let blocks = Array.from(blockscontainer.children);
+
+
+let orderRange = [...Array(blocks.length).keys()];
